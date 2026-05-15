b21be8e4a8c8844ec7e4dbc8bc46daac03545315
Rinenn <isaac.rinen@gmail.com>
2026-05-15 19:33:08 +0300

    Frontend + backend skeleton

---
+++ frontend/template/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>MwiDrive</h1>
+            <p>Where ever you wanna go offline, we got you</p>
+        </div>
+    )
+}
+
+export default App
